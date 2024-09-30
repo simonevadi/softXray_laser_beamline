e970602e63caf6124698587792f3d8deb586949f
Sebastian Malerz <sebastian.malerz@helmholtz-berlin.de>
2024-09-30 11:34:43 +0200

    final design No. 9 - sims not finish

---
+++ Simulations/9_Hitachi_Shimadzu_1200_final/parameter_bw.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+nrays    = 1e5
+rounds   = 1400
+ncpu     = 12
+
+SlitSize = np.array([0.14])
+energy   = np.array([120])
+energySpread = np.array([160])      #means half the value arround the set energy
+lin_polarization = np.array([1])
+
+
+rml_file_name_Laser_BL = 'Laser_Shimadzu_Hitachi_1200'
+
+
+### plotting colors
+import matplotlib
+import matplotlib.pyplot as plt
+# Generate 20 colors from the 'hsv' colormap which resembles a rainbow
+colors_rainbow = plt.cm.tab20(np.linspace(0, 2, int(max(1,2))))
+# Convert the colors to hex format for easy usage
+colors = [matplotlib.colors.rgb2hex(color) for color in colors_rainbow]
+colors = ["Red", "Orange", "Green", "Blue", "Indigo", "Violet"]
---
+++ Simulations/9_Hitachi_Shimadzu_1200_final/simulations_bw_rp.py
@@ -0,0 +1,58 @@
+from raypyng import Simulate
+import numpy as np
+import os
+
+from helper_functions import delete_round_folders
+
+from parameter_bw import nrays, rounds, ncpu
+from parameter_bw import energy, SlitSize, lin_polarization, energySpread
+from parameter_bw import rml_file_name_Laser_BL as rml_file_name
+#from parameter import lin_polarization
+
+
+# work out absolute path of rml file
+rml_file_path = os.path.join('rml', rml_file_name+'.rml')
+
+sim = Simulate(rml_file_path, hide=True)
+
+beamline = sim.rml.beamline
+
+
+# name for simulation folder
+sim_name = rml_file_name+'bw_RP'
+
+# define a list of dictionaries with the parameters to scan
+# and plug them into the Simulation class
+
+sim.params = [              
+            {beamline.ExitSlit.openingHeight:SlitSize},
+            {beamline.Laser.photonEnergy:energy},
+            {beamline.Laser.numberRays:nrays},
+            {beamline.Laser.linearPol_0:lin_polarization},
+            {beamline.Laser.energySpread:energySpread}
+        ]
+
+# sim.simulation_folder = '/home/simone/Documents/RAYPYNG/raypyng/test'
+sim.simulation_name = sim_name
+
+# turn off reflectivity
+sim.reflectivity(reflectivity=False)
+
+# repeat the simulations as many time as needed
+sim.repeat = rounds
+
+sim.analyze = False # don't let RAY-UI analyze the results
+sim.raypyng_analysis=True # let raypyng analyze the results
+
+## This must be a list of dictionaries
+sim.exports  =  [
+    {beamline.CamInput:['RawRaysOutgoing']}
+    ]
+
+
+# create the rml files
+#sim.rml_list()
+
+#uncomment to run the simulations
+sim.run(multiprocessing=ncpu, force=False)
+# delete_round_folders('RAYPy_Simulation_'+sim_name)
